ef0b7d57f79b899a0a17b4e45333931d8413b743
simsonites <38902001+simsonites@users.noreply.github.com>
2020-11-29 04:03:49 +0700

    Project skeleton first commit

---
+++ src/main/java/com/softpager/emedicare/EmedicareApplication.java
@@ -0,0 +1,13 @@
+package com.softpager.emedicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmedicareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmedicareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softpager/emedicare/EmedicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softpager.emedicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmedicareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
